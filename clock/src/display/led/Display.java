5fc8f8e1fee6f28dbd48dc1d9f8cffebf790d808
yjfiejd <yjfiejd@163.com>
2018-06-01 16:04:16 +0800

    add clock_practice

---
+++ clock/src/display/led/Display.java
@@ -0,0 +1,40 @@
+package display.led;
+
+public class Display {
+	private int value = 0;
+	private int limit = 0;
+	
+	public Display(int limit)
+	{
+		this.limit = limit;
+	}
+	
+	public void increase()
+	{
+		value++;
+		if (value == limit)
+		{
+			value = 0;
+		}
+	}
+	
+	public int getValue()
+	{
+		return value;
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Display d = new Display(24);
+//		d.value ++;
+		for(int i=0;;)
+		{
+			
+			System.out.println(d.getValue());
+			d.increase();
+		}
+	}
+
+}
